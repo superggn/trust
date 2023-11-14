1176ce79fbf2ee53030659c9619fe3b6b2ec93fe
superggn <939401399@qq.com>
2023-11-14 18:53:19 +0800

    temp

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::io;
 fn main() -> io::Result<()> {
-    let nic = tun_tap::Iface::new("my_tun0", tun_tap::Mode::Tun).expect("failed to create");
+    let nic = tun_tap::Iface::without_packet_info("my_tun0", tun_tap::Mode::Tun)
+        .expect("failed to create");
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        println!("flags: {}", flags);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        println!("flags: {}", proto);
+        eprintln!("read {} bytes: {:x?}", nbytes - 4, &buf[4..nbytes]);
         println!("nic: {:?}", nic);
     }
     Ok(())

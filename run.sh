375d20f1a21bbc26626299ceb49b2fb95f75d575
superggn <939401399@qq.com>
2023-11-16 10:33:20 +0800

    temp

--- run.sh
+++ run.sh
@@ -4,6 +4,12 @@ sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR/release/trust
 
 $CARGO_TARGET_DIR/release/trust &
 pid=$!
+ext=$?
+
+if [[ $ext -ne 0 ]]; then
+    exit $ext
+fi
+
 sudo ip addr add 192.168.0.1/24 dev my_tun0
 sudo ip link set up dev my_tun0
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
+use std::collections::HashMap;
 use std::io;
+use std::net::Ipv4Addr;
+
+mod tcp;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
 fn main() -> io::Result<()> {
+    let mut connections: HashMap<Quad, tcp::State> = Default::default();
     let nic = tun_tap::Iface::new("my_tun0", tun_tap::Mode::Tun).expect("failed to create");
     let mut buf = [0u8; 1504];
     loop {
+        // 每次 recv 就是往 buf 里面填充数据帧
         let nbytes = nic.recv(&mut buf[..])?;
         let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
         let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
@@ -11,26 +23,33 @@ fn main() -> io::Result<()> {
             // if current packet is not ipv4: continue
             continue;
         }
+        // &buf[4..nbytes] 是网络层数据帧， 从网络层数据帧能扒出来 ipv4 header
+        // ipv4 header 里面的 protocol number 会说明 传输层用啥协议
         match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
-            Ok(p) => {
-                let src = p.source_addr();
-                let dst = p.destination_addr();
-                let proto = p.protocol();
-                eprintln!(
-                    "{} - {} {}b of protocol {} ",
-                    src,
-                    dst,
-                    p.payload_len(),
-                    proto,
-                );
-                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + p.slice().len()..]) {
-                    Ok(p) => {
+            Ok(ip_header) => {
+                let src = ip_header.source_addr();
+                let dst = ip_header.destination_addr();
+                let tx_layer_proto = ip_header.protocol();
+                if tx_layer_proto != 0x06 {
+                    continue;
+                };
+                // &buf[4 + ipv4_header.slice().len()..nbytes] 是传输层数据帧， 能扒出来 tcp header
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + ip_header.slice().len()..]) {
+                    Ok(tcp_header) => {
+                        let data_index = 4 + ip_header.slice().len() + tcp_header.slice().len();
+                        connections
+                            .entry(Quad {
+                                src: (src, tcp_header.source_port()),
+                                dst: (dst, tcp_header.destination_port()),
+                            })
+                            .or_default()
+                            .on_packet(ip_header, tcp_header, &buf[data_index..]);
                         eprintln!(
                             "{} - {} {}b of tcp to port {} ",
                             src,
                             dst,
-                            p.slice().len(),
-                            p.destination_port(),
+                            tcp_header.slice().len(),
+                            tcp_header.destination_port(),
                         );
                     }
                     Err(e) => {
